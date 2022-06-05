e316ed2c0daebe092d86790980ed24c42b744e51
nouman <nomi676767@gmail.com>
2022-06-05 01:20:58 -0700

    first commit

---
+++ assets/js/theme.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#open-sidebar-menu").click(function () {
+    console.log("heloo");
+    $("#user-sidebar-menu-wrapper").toggle(4000);
+  });
+  $("#close-icon-sidebar").click(function () {
+    $("#user-sidebar-menu-wrapper").toggle(4000);
+  });
+})
